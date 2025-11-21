50c329ada55f31b614712caafd238d68ef92e40c
Kenneth Obsequio <k80308392@gmail.com>
2025-11-22 06:01:33 +0800

    core: add PATCH to CORS
    I forgot to include my bad.
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const unifiedCors = {
     "http://localhost:5173"
   ],
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Accept"]
 };
 
