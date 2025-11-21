aa1e4aa3f6f900101fe3fa4fa333cd066baaf8b7
Kenneth Obsequio <k80308392@gmail.com>
2025-11-21 12:36:13 +0800

    CORS added
    Frontend naa
    
    Signed-off-by: Kenneth Obsequio <k80308392@gmail.com>

--- server.js
+++ server.js
@@ -18,8 +18,13 @@ app.set('views', './views');
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+    origin: [
+      "https://barangay-santa-monica.vercel.app",
+      "http://localhost:5173",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
   },
   transports: ["websocket"],
 });
